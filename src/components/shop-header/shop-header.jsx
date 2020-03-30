3de84a66b622be95fcf47e9a3099ace9b13732f2
FyuroTEX <yurilev4@gmail.com>
2020-03-31 00:54:50 +0300

    add loading spinner

--- src/components/shop-header/shop-header.jsx
+++ src/components/shop-header/shop-header.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import './shop-header.css';
+import { Link } from 'react-router-dom'
+
 
 
 const ShopHeader = ({ numItems, total }) => {
     return (
         <header className='shop-header row'>
-            <a href="/" className='logo text-dark'>ReStore</a>
-            <a href="/" className="shopping-cart">
+            <Link to="/" className='logo text-dark'>ReStore</Link>
+            <Link to="/cart" className="shopping-cart">
                 <i className="fa fa-shopping-cart" />
                 &nbsp;
                 {numItems} items (${total})
-            </a>
+            </Link>
         </header>
     );
 };
